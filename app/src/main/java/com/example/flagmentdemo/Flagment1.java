526ae7a46aea23d23aea65b095e31d91073c5a9b
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-03-22 11:31:18 +0700

    'ver1'

---
+++ app/src/main/java/com/example/flagmentdemo/Flagment1.java
@@ -0,0 +1,25 @@
+package com.example.flagmentdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Flagment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Flagment1 extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_flagment1, container, false);
+        return  view;
+    }
+}
---
+++ app/src/main/java/com/example/flagmentdemo/Flagment2.java
@@ -0,0 +1,25 @@
+package com.example.flagmentdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Flagment2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Flagment2 extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_flagment2, container, false);
+        return view;
+    }
+}
